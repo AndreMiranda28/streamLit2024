3936efc1a13eae2a2ff1a156a30c377c641600d3
ANDRE ANTONIO MIRANDA <93847361+AndreMiranda28@users.noreply.github.com>
2024-02-03 23:18:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import streamlit  as st
 import pandas as pd
 import numpy  as np
+
+
+st.title('2024 PRESENTACION')
+st.header('Simulador FVentas')
+n = st.slider("cant. ventas",5,100,step = 1)
+chart_data = pd.DataFrame(np.random.randn(n),columns=['ventas'])
+st.line_chart(chart_data)
